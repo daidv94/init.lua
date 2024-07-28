710882117cfaa730ba075bb789cde4638a792ec7
daidv94 <dai.dao@noreply.github.io>
2024-07-28 09:35:45 +0700

    update auto cmp
    Signed-off-by: daidv94 <dai.dao@noreply.github.io>

--- init.lua
+++ init.lua
@@ -3,4 +3,3 @@ require("config.keymaps")
 require("config.options")
 require("config.autocmds")
 require("config.lazy")
-
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,6 +7,8 @@ local format = augroup("Format", {})
 
 local yank = augroup("HighLightYank", {})
 
+local lsp = augroup("LspAttach", {})
+
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("TextYankPost", {
@@ -26,3 +28,19 @@ autocmd({ "BufWritePre" }, {
   command = [[%s/\s\+$//e]],
 })
 
+autocmd('LspAttach', {
+  group = lsp,
+  callback = function(e)
+    local opts = { buffer = e.buf }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  end
+})
