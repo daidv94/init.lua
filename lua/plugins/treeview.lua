21a7e04cdf754817b7058f50c4d9c132890f84b9
Danny Dao <danny.dao@geocomply.com>
2024-08-01 15:02:57 +0700

    adjust treeview
    Signed-off-by: Danny Dao <danny.dao@geocomply.com>

--- lua/plugins/treeview.lua
+++ lua/plugins/treeview.lua
@@ -6,10 +6,19 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      git = {
+        ignore = false,
+      },
+      filters = {
+        dotfiles = false,
+      },
+      view = {
+        adaptive_size = true,
+      },
+    }
     vim.keymap.set('n', '<leader>ee', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
     vim.keymap.set('n', '<leader>er', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
     vim.keymap.set('n', '<leader>ec', ':NvimTreeCollapse<CR>', { noremap = true, silent = true })
   end,
 }
-
