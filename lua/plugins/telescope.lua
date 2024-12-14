b199d95ebf84647ddc4d37ddefebd7cfef1f3d07
daidv94 <dai.dao@noreply.github.io>
2024-12-14 12:51:32 +0700

    feat: add custom multi grep with file extension
    Signed-off-by: daidv94 <dai.dao@noreply.github.io>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -35,14 +35,13 @@ return {
       local word = vim.fn.expand("<cWORD>")
       builtin.grep_string({ search = word })
     end)
-    vim.keymap.set("n", "<leader>ss", function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end)
+    vim.keymap.set("n", "<leader>sl", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
     vim.keymap.set("n", "<leader>sm", builtin.marks, {})
     vim.keymap.set("n", "<leader>sp", builtin.oldfiles, {})
     vim.keymap.set("n", "<leader>sq", builtin.quickfix, {})
-    vim.keymap.set("n", "<leader>sl", builtin.loclist, {})
-    vim.keymap.set("n", "<leader>sj", builtin.jumplist, {})
+
+    -- Custom live grep, can be passed arguments into it
+    require("config.telescope.multigrep").setup()
   end,
 }
